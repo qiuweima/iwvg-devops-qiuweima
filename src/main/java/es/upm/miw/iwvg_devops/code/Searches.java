153226c8ea9d15f474276d52a8468fd57f284604
qiuweima <1147059908@qq.com>
2023-09-28 19:08:38 +0200

    added search 4#10

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -10,7 +10,16 @@ import java.util.Optional;
                             .map(fraction -> (double) fraction.getNumerator() / fraction.getDenominator())
                     );
         }
-
+        public Double findFirstDecimalFractionByUserName(String name) {
+            Optional<Double> result = new UsersDatabase().findAll()
+                    .filter(user -> name.equals(user.getName()))
+                    .flatMap(user -> user.getFractions().stream()
+                            .filter(fraction -> fraction.getDenominator() != 0)
+                            .map(fraction -> (double) fraction.getNumerator() / fraction.getDenominator())
+                    )
+                    .findFirst();
+            return result.orElse(null);
+        }
     }
 
 
--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -2,7 +2,7 @@ package es.upm.miw.iwvg_devops.code;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +19,10 @@ class SearchesTest {
     void testFindDecimalImproperFractionByUserName() {
         assertEquals(List.of(2.0, 1.3333333333333333), searches.findDecimalImproperFractionByUserName("Ana")
                 .collect(Collectors.toList()));
+    }
+    @Test
+    void testFindFirstDecimalFractionByUserName() {
+        assertEquals(0.0, searches.findFirstDecimalFractionByUserName("Oscar"));
     }
 }
 
